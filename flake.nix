56da0b6da95d3031135633b41eacaeebcd5cdc30
tchz <theodoros.chatzi@tremau.com>
2024-03-28 23:19:32 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Arcade theme for grub";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/unstable;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "arcade-grub-theme";
+        src = self;
+        installPhase = "cp -r Arcade $out";
+      };
+  };
+}
